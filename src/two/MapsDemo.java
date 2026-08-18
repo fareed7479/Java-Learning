7aefeba39aa3ad8f3d5901bc882057e55104872a
fareed7479 <fareedsk2233@gmail.com>
2026-08-18 12:08:39 +0530

    Learned and noted on the Generics

---
+++ src/two/MapsDemo.java
@@ -0,0 +1,23 @@
+package two;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapsDemo {
+    public static void main(String[] args) {
+        Map<Integer , String> map = new HashMap<>();
+        //Wrapper Classes -
+        //The Primitive Dataypes can't be used as objects.
+        //So int is used as Wrapper Class of Integer.
+        //Since Map takes objects as Inputs.
+        //Maps don't have duplicates.
+        //Maps have unique Key Value's
+
+        map.put(1, "Fareed");
+        map.put(2, "Vijay");
+        map.put(3, "Yaswanth");
+
+        System.out.println("User with id 2 is: " + map.get(2));
+    }
+}
+
--- src/two/SetDemo.java
+++ src/two/SetDemo.java
@@ -1,9 +1,25 @@
 package two;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetDemo {
 
     /*Set is a DataStructure which Doesn't Allow
     duplicate Objects and Elements.
     -Set is an Interfcae
      */
+
+    public static void main(String[] args) {
+        Set<String> roles = new HashSet<>();
+        roles.add("ADMIN");
+        roles.add("Manager");
+        roles.add("User");
+        roles.add("Manager");
+        //if duplicate is there , it doesn't add into the set
+        //but there is no error for that and doesn't refelect on set.
+        for(String role : roles){
+            System.out.println(role);
+        }
+    }
 }
